b680d27c56e997157399c997f96e899f57e3e33d
shuabruh <shuabruh@student.telkomuniversity.ac.id>
2024-03-23 12:40:39 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -2,6 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract - Joshua Daniel");
+        vid.printVideoDetails();
+    }
+}
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.title = title;
+        Random randomId = new Random();
+        id = randomId.Next(10000, 99999);
+    }
+
+    public void increasePlayCount(int increment)
+    {
+        playCount += increment;
+    }
+
+    public void printVideoDetails()
+    {
+        Console.WriteLine("ID: " + id + "\n" + "Judul: " + title + "\n" + "Play count: " + playCount + "\n");
     }
 }
